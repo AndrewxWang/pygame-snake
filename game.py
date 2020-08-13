54d64240da2075380300d81a4a80fa91840333f5
Andrew Wang <57112250+AndrewxWang@users.noreply.github.com>
2020-08-13 15:07:38 -0400

    Update game.py
    fixed issues with movement
    added snake length
    fixed apple position issues

--- game.py
+++ game.py
@@ -5,42 +5,59 @@ pygame.init()
 
 win = pygame.display.set_mode((500,500))
 pygame.display.set_caption("Snake")
-
+print("Snake Remake by Andrew Wang")
 x = 240
 y = 240
 width = 20
 height = 20
-vel = 10
+vel = 20
 x_move = 0
 y_move = 0
 score = 0
+highscore = 0
 gameover = False
 play_again = True
 font = pygame.font.SysFont("Arial Black", 17)
-randx, randy = round(random.randint(20,460) / 10) * 10, round(random.randint(20,460) / 10) * 10
+snake = []
+
+def randxy():
+    randx, randy = -1, -1
+    while randx % 20 != 0: 
+        randx = round(random.randint(20,460) / 10) * 10
+    while randy % 20 != 0: 
+        randy = round(random.randint(20,460) / 10) * 10
+
+    return randx, randy
+randx, randy = randxy()
+
+def drawSnake():
+    global snake
+    for x1 in snake:
+        pygame.draw.rect(win, (255,255,255), (x1[0], x1[1], width, height))
 
 def drawApple():
     global randx, randy, score
-##    print(str(x) + " " + str(y))
-##    print(str(randx) + " " + str(randy))
     pygame.draw.rect(win, (255,0,0), (randx, randy, width, height))
     if randx == x and randy == y:
-        print("yes")
-        randx, randy = round(random.randint(20,460) / 10) * 10, round(random.randint(20,460) / 10) * 10
+        randx, randy = randxy()
         score+=1
     
 def resetStats():
-    global x, y ,x_move, y_move, score, randx, randy
+    global x, y ,x_move, y_move, score, randx, randy, snake
     x = 240
     y = 240
     x_move = 0
     y_move = 0
     score = 0
-    randx, randy = round(random.randint(20,460) / 10) * 10, round(random.randint(20,460) / 10) * 10
+    randx, randy = randxy()
+    snake.clear()
 
 def displayScore():
     label = font.render("Score: " + str(score), 1, (255,255,255))
     win.blit(label, (0, 0))
+    label2 = font.render("High Score: " + str(highscore), 1, (255,255,255))
+    win.blit(label2, (350, 0))
+    
     pygame.display.update()
 
 def drawBorders():
@@ -58,47 +75,71 @@ def checkDeath():
 
 while play_again:
     if not gameover:
-        pygame.time.delay(40)
+        pygame.time.delay(60)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 gameover = True
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
+                if event.key == pygame.K_LEFT and x_move != vel:
                     x_move = -vel
                     y_move = 0
-                elif event.key == pygame.K_RIGHT:
+                elif event.key == pygame.K_RIGHT and x_move != -vel:
                     x_move = vel
                     y_move = 0
-                elif event.key == pygame.K_UP:
-                    x_move = 0
+                elif event.key == pygame.K_UP and y_move != vel:
                     y_move = -vel
-                elif event.key == pygame.K_DOWN:
                     x_move = 0
+
+                elif event.key == pygame.K_DOWN and y_move != -vel:
                     y_move = vel
+                    x_move = 0
+                elif event.key == pygame.K_r:
+                    resetStats()
         x+=x_move
         y+=y_move
         win.fill((0,0,0))
         gameover = checkDeath()
         drawBorders()
         pygame.draw.rect(win, (255,255,255), (x, y, width, height))
+        
         drawApple()
         if x_move == 0 and y_move == 0:
             label = font.render("Use the arrow keys to move", 1, (255,255,255))
             win.blit(label, (125, 200))
+            
+        if score > highscore:
+            highscore = score
         displayScore()
+
+        snakeLen = []
+        snakeLen.append(x)
+        snakeLen.append(y)
+        snake.append(snakeLen)
+
+        if len(snake) > score+1:
+            del snake[0]
+
+        for x1 in snake[:-1]:
+            if x1 == snakeLen:
+                gameover = True
+        drawSnake()
+        
         pygame.display.update()
     else:
-        pygame.time.delay(30)
-        
         pygame.draw.rect(win, (255,255,0), (x, y, width, height))
+        pygame.time.delay(30)
         font2 = pygame.font.SysFont("Arial Black", 50)
         label = font2.render("GAME OVER!", 1, (255,255,255))
         label2 = font.render("Press R to restart or X to exit", 1,  (255,255,255))
+        label_1 = font2.render("GAME OVER!", 1, (0,0,0))
+        label_2 = font.render("Press R to restart or X to exit", 1,  (0,0,0))
+        win.blit(label_1, (76,201))
+        win.blit(label_2, (76,276))
         win.blit(label, (75, 200))
         win.blit(label2, (75,275))
         pygame.display.update()
- 
+            
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 gameover = True
@@ -107,5 +148,8 @@ while play_again:
                     resetStats()
                     gameover = False
                 elif event.key == pygame.K_x:
-                    play_again = False    
+                    play_again = False
+print("Thanks for playing!")
+time.sleep(2)
+
 pygame.quit()
